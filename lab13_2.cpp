b2d631803dc8f8ed45052fff61d425b5785720cd
zewexqa <154309211+zewexqa@users.noreply.github.com>
2024-01-02 16:43:55 +0700

    Update lab13_2.cpp

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,33 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool im[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for (int i = 0 ; i < N; i++){
+        cout << "|";
+        for (int j = 0; j < M; j++ ){
+            if(im[i][j]==1){
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
+
+
+}
+
+void updateImage(bool im[][M] ,int s ,int x ,int y){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1){
+                im[i][j] = 1 ;
+            }
+        }
+
+    }
+}
